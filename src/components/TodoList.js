d701c40473c6d95eb7eee82749f72b154c811aec
rensilva5 <renzoasilva@gmail.com>
2022-10-18 22:54:48 -0400

    first part is done, delete and completed button are working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Todo from "./Todo";
 
-function TodoList() {
+function TodoList({ todos, setTodos }) {
   return (
-    <div className='todo-container'>
-        <ul className='todo-list'></ul>
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+            id={todo.id}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default TodoList;
